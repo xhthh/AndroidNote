3ebd91d75c6c7d3e3abfc51a6396a8d52e73aa64
xhthh <1399535513@qq.com>
2018-09-02 17:54:12 +0800

    ...

--- app/src/main/java/com/xht/androidnote/MainActivity.java
+++ app/src/main/java/com/xht/androidnote/MainActivity.java
@@ -1,7 +1,5 @@
 package com.xht.androidnote;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.View;
 
 import com.xht.androidnote.base.BaseActivity;
@@ -36,12 +34,6 @@ public class MainActivity extends BaseActivity {
 
         /*Singleton instance = Singleton.getInstance();
         instance.doSomething();*/
-
-    }
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
     }
 
