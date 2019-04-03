95d0c56128413887dc47c34aeb4b98b992299e35
许海涛 <xuhaitao@autohome.com.cn>
2019-04-03 09:26:19 +0800

    获取屏幕宽高的方法

--- app/src/main/java/com/xht/androidnote/utils/CommonUtils.java
+++ app/src/main/java/com/xht/androidnote/utils/CommonUtils.java
@@ -1,6 +1,12 @@
 package com.xht.androidnote.utils;
 
+import android.content.Context;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Environment;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
 
 import java.io.BufferedReader;
 import java.io.Closeable;
@@ -103,5 +109,55 @@ public class CommonUtils {
         }
     }
 
+
+    public static int[] getScreenSize(Context context) {
+        int[] size = new int[2];
+
+        WindowManager w = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display d = w.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        d.getMetrics(metrics);
+        // since SDK_INT = 1;
+        int widthPixels = metrics.widthPixels;
+        int heightPixels = metrics.heightPixels;
+
+        // includes window decorations (statusbar bar/menu bar)
+        if (Build.VERSION.SDK_INT >= 14 && Build.VERSION.SDK_INT < 17)
+            try {
+                widthPixels = (Integer) Display.class.getMethod("getRawWidth").invoke(d);
+                heightPixels = (Integer) Display.class.getMethod("getRawHeight").invoke(d);
+            } catch (Exception ignored) {
+            }
+        // includes window decorations (statusbar bar/menu bar)
+        if (Build.VERSION.SDK_INT >= 17)
+            try {
+                Point realSize = new Point();
+                Display.class.getMethod("getRealSize", Point.class).invoke(d, realSize);
+                widthPixels = realSize.x;
+                heightPixels = realSize.y;
+            } catch (Exception ignored) {
+            }
+        size[0] = widthPixels;
+        size[1] = heightPixels;
+        return size;
+    }
+
+    /**
+     * 获取屏幕宽度
+     * @param context
+     * @return
+     */
+    public static int getScreenWidth(Context context) {
+        return getScreenSize(context)[0];
+    }
+
+    /**
+     * 获取屏幕高度
+     * @param context
+     * @return
+     */
+    public static int getScreenHeight(Context context) {
+        return getScreenSize(context)[1];
+    }
 
 }
