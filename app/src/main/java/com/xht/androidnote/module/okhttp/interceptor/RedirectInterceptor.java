d62519cd899a550f8f6414540aacb214312f4d9e
许海涛 <xuhaitao@autohome.com.cn>
2019-09-27 11:19:19 +0800

    rxjava

---
+++ app/src/main/java/com/xht/androidnote/module/okhttp/interceptor/RedirectInterceptor.java
@@ -0,0 +1,17 @@
+package com.xht.androidnote.module.okhttp.interceptor;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Response;
+
+/**
+ * Created by xht on 2019/9/9.
+ */
+public class RedirectInterceptor implements Interceptor {
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        return null;
+    }
+}
--- app/src/main/java/com/xht/androidnote/module/rxjava/RxJavaActivity.java
+++ app/src/main/java/com/xht/androidnote/module/rxjava/RxJavaActivity.java
@@ -1,7 +1,12 @@
 package com.xht.androidnote.module.rxjava;
 
+import android.util.Log;
+
 import com.xht.androidnote.R;
 import com.xht.androidnote.base.BaseActivity;
+
+import io.reactivex.Flowable;
+import io.reactivex.functions.Consumer;
 
 /**
  * Created by xht on 2018/6/6.
@@ -15,6 +20,15 @@ public class RxJavaActivity extends BaseActivity {
 
     @Override
     protected void initEventAndData() {
+        test1();
+    }
 
+    private void test1() {
+        Flowable.just("Hello World").subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                Log.i("xht","s===" + s);
+            }
+        });
     }
 }
