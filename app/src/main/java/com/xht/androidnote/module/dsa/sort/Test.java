cd3141b8df9ced58e73d814ef9efe372e96b2bc4
许海涛 <xuhaitao@autohome.com.cn>
2020-01-03 11:29:05 +0800

    鸡尾酒排序

--- app/src/main/java/com/xht/androidnote/module/dsa/sort/Test.java
+++ app/src/main/java/com/xht/androidnote/module/dsa/sort/Test.java
@@ -11,13 +11,20 @@ public class Test {
     public static void main(String[] args) {
         Test test = new Test();
 
-        int[] array = new int[]{5, 8, 6, 3, 9, 2, 1, 7};
+        /*int[] array = new int[]{5, 8, 6, 3, 9, 2, 1, 7};
 
         System.out.println(Arrays.toString(array) + "\n");
 
         test.test1(array);
 
-        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(array));*/
+
+
+        int[] array = new int[]{2, 3, 4, 5, 6, 7, 8, 1};
+
+        System.out.println(Arrays.toString(array) + "\n");
+        test.tes4(array);
+        System.out.println(Arrays.toString(array) + "\n");
 
 
     }
@@ -108,7 +115,57 @@ public class Test {
     }
 
 
+    /**
+     * {2,3,4,5,6,7,8,1}
+     *
+     * @param array
+     */
+    public void tes4(int[] array) {
 
+        int temp = 0;
+
+        for (int i = 0; i < array.length / 2; i++) {
+            //有序标记，每一轮的初始值都是true
+            boolean isSorted = true;
+            //奇数轮，从左向右比较交换
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+
+                    //有元素交换，所以不是有序的，标记置为false
+                    isSorted = false;
+                }
+            }
+
+            if (isSorted) {
+                break;
+            }
+
+            //在偶数轮之前，将isSorted重新标记为true
+            isSorted = true;
+
+            //偶数轮，从右向左比较和交换
+            for (int j = array.length - i - 1; j > i; j--) {
+                if (array[j] < array[j - 1]) {
+                    temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+
+                    //因为有元素进行交换，所以不是有序的，标记变为false
+                    isSorted = false;
+                }
+
+            }
+
+            if (isSorted) {
+                break;
+            }
+        }
+
+
+    }
 
 
 }
