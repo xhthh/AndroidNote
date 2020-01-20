a8ee399f86fb97fe10a11c31e655276e3105eddc
许海涛 <xuhaitao@autohome.com.cn>
2020-01-20 15:12:28 +0800

    归并，快速

--- app/src/main/java/com/xht/androidnote/module/dsa/sort/Test4.java
+++ app/src/main/java/com/xht/androidnote/module/dsa/sort/Test4.java
@@ -8,20 +8,56 @@ import java.util.Arrays;
 public class Test4 {
 
 
+    /*
+        自顶向下的归并排序
+     */
+    private static void sort(int[] array, int low, int high) {
 
-    public static void test(int[] array) {
+        int mid = (low + high) / 2;
 
+        if (low < high) {
+            sort(array, low, mid);
+            sort(array, mid + 1, high);
+            merge(array, low, mid, high);
+        }
 
+    }
 
+    private static void merge(int[] array, int low, int mid, int high) {
 
+        int[] temp = new int[high - low + 1];
+        int left = low;
+        int right = mid + 1;
+        int k = 0;
 
+        while (left <= mid && right <= high) {
+            if(array[left] < array[right]) {
+                temp[k++] = array[left++];
+            } else {
+                temp[k++] = array[right++];
+            }
+        }
+
+        while (left <= mid) {
+            temp[k++] = array[left++];
+        }
+
+        while (right <= high) {
+            temp[k++] = array[right++];
+        }
+
+        for (int i = 0; i < temp.length; i++) {
+            array[i + low] = temp[i];
+        }
 
     }
 
 
     public static void main(String[] args) {
         int[] array = new int[]{5, 8, 6, 3, 9, 2, 1, 7};
-        //test1(array);
+        sort(array, 0, array.length - 1);
         System.out.println(Arrays.toString(array));
     }
+
+
 }
