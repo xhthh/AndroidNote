98ab44c09cbb2356ab541a961057848e14c94fd0
许海涛 <xuhaitao@autohome.com.cn>
2020-06-02 11:07:50 +0800

    EditText测试

--- app/src/main/java/com/xht/androidnote/module/view/edittext/EditTextActivity.java
+++ app/src/main/java/com/xht/androidnote/module/view/edittext/EditTextActivity.java
@@ -2,12 +2,16 @@ package com.xht.androidnote.module.view.edittext;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.xht.androidnote.R;
 import com.xht.androidnote.base.BaseActivity;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * Created by xht on 2020/1/9.
@@ -15,6 +19,10 @@ import butterknife.BindView;
 public class EditTextActivity extends BaseActivity {
     @BindView(R.id.edit_text_test1)
     EditText editTextTest1;
+    @BindView(R.id.iv_back)
+    ImageView ivBack;
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
 
     @Override
     protected int getLayoutId() {
@@ -24,26 +32,35 @@ public class EditTextActivity extends BaseActivity {
     @Override
     protected void initEventAndData() {
 
-     editTextTest1.addTextChangedListener(new PriceWatcher(editTextTest1));
+        editTextTest1.addTextChangedListener(new PriceWatcher(editTextTest1));
 
 
+        editTextTest1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-     editTextTest1.addTextChangedListener(new TextWatcher() {
-         @Override
-         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
-         }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-         @Override
-         public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
 
-         }
+            @Override
+            public void afterTextChanged(Editable s) {
 
-         @Override
-         public void afterTextChanged(Editable s) {
-
-         }
-     });
+            }
+        });
     }
 
+    @OnClick({R.id.iv_back, R.id.tv_title})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.iv_back:
+                finish();
+                break;
+            case R.id.tv_title:
+                break;
+        }
+    }
 }
