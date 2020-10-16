49c1090ad663114d9cc16a7e56df0fb2eab6bb7b
xuhaitao <xuhaitao@9fbank.com.cn>
2020-10-16 17:37:23 +0800

    rename

--- app/src/main/java/com/xht/androidnote/summarize/test/GenericsTest.java
+++ app/src/main/java/com/xht/androidnote/summarize/test/GenericsTest.java
@@ -1,6 +1,8 @@
 package com.xht.androidnote.summarize.test;
 
-public class JavaTest {
+import java.util.ArrayList;
+
+public class GenericsTest {
 
     public static void main(String[] args) {
 
@@ -18,6 +20,21 @@ public class JavaTest {
 
         new Person<String>(233);
 
+
+        ArrayList<String> list1 = new ArrayList();
+        list1.add("1"); //编译通过
+//        list1.add(1); //编译错误
+        String str1 = list1.get(0); //返回类型就是String
+
+        ArrayList list2 = new ArrayList<String>();
+        list2.add("1"); //编译通过
+        list2.add(1); //编译通过
+        Object object = list2.get(0); //返回类型就是Object
+
+        new ArrayList<String>().add("11"); //编译通过
+//        new ArrayList<String>().add(22); //编译错误
+
+        String str2 = new ArrayList<String>().get(0); //返回类型就是String
     }
 
     /**
