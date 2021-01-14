38f41faaea2010822f93f69f7a0527111e1a180a
xuhaitao <xuhaitao@9fbank.com.cn>
2021-01-14 18:26:58 +0800

    OkHttp test

---
+++ app/src/main/java/com/xht/androidnote/module/okhttp/scode/OkHttpTestActivity.kt
@@ -0,0 +1,33 @@
+package com.xht.androidnote.module.okhttp.scode
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.xht.androidnote.R
+import okhttp3.*
+import java.io.IOException
+
+class OkHttpTestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_okhttp_test)
+
+        test1()
+    }
+
+    private fun test1() {
+        val client = OkHttpClient().newBuilder().build()
+        val request = Request.Builder().url("https://www.baidu.com").build()
+        val call = client.newCall(request)
+        call.enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("onFailure:$e")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                println("onResponse:${response.body?.string()}")
+            }
+        })
+    }
+
+}
