48e30fe6a2782dd2ca114b52edfea7b86533d203
xuhaitao <xuhaitao@9fbank.com.cn>
2023-05-30 11:03:04 +0800

    kotlin test

--- app/src/main/java/com/xht/androidnote/module/kotlin/KTTest.kt
+++ app/src/main/java/com/xht/androidnote/module/kotlin/KTTest.kt
@@ -252,6 +252,7 @@ fun main() {
 
     people("Android") { say("World") }
 
+    //Kotlin 中 双冒号操作符 表示把一个方法当做一个参数，传递到另一个方法中进行使用，通俗的来讲就是引用一个方法。
     println(lock("param1", "param2", ::getResult))
 }
 
