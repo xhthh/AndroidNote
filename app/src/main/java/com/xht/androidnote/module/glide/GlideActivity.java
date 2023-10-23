4fba2f1a91d9607a5b7240b52d151afcc3c22bf8
xuhaitao <xuhaitao@9fbank.com.cn>
2023-10-23 18:00:33 +0800

    更新glide

--- app/src/main/java/com/xht/androidnote/module/glide/GlideActivity.java
+++ app/src/main/java/com/xht/androidnote/module/glide/GlideActivity.java
@@ -3,11 +3,20 @@ package com.xht.androidnote.module.glide;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.xht.androidnote.R;
 import com.xht.androidnote.base.BaseActivity;
 
@@ -25,7 +34,8 @@ public class GlideActivity extends BaseActivity {
     @BindView(R.id.iv_glide_test1)
     ImageView mIvGlideTest1;
 
-    private String url = "http://cn.bing.com/az/hprichbg/rb/Dongdaemun_ZH-CN10736487148_1920x1080.jpg";
+    private String url = "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png";
+    //private String url = "http://cn.bing.com/az/hprichbg/rb/Dongdaemun_ZH-CN10736487148_1920x1080.jpg";
     private String gif = "http://p1.pstatp.com/large/166200019850062839d3";
 
     @Override
@@ -41,7 +51,24 @@ public class GlideActivity extends BaseActivity {
     @OnClick(R.id.btn_load)
     public void onViewClicked() {
 
-        Glide.with(this).load(url).into(mIvGlideTest1);
+        Glide.with(this)
+                .load(url)
+                .error(R.mipmap.ic_launcher)
+                .placeholder(R.mipmap.splash)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, @Nullable Object model, @NonNull Target<Drawable> target, boolean isFirstResource) {
+                        Log.e("Glide","------onLoadFailed()---e.message = " + e.getMessage());
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(@NonNull Drawable resource, @NonNull Object model, Target<Drawable> target, @NonNull DataSource dataSource, boolean isFirstResource) {
+                        Log.e("Glide","------onResourceReady()------");
+                        return false;
+                    }
+                })
+                .into(mIvGlideTest1);
 
 
         /*GlideApp.with(mContext)
